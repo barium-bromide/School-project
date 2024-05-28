5d9299ae86908c50b31ee85080220b5a9039d4ae
ddddddddrrdd3 <108572893+ddddddddrrdd3@users.noreply.github.com>
2024-05-28 15:26:16 +0800

    place to login js

--- public/js/login.js
+++ public/js/login.js
@@ -8,6 +8,36 @@ const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("loginbtn");
 const teacherForm = document.getElementById("teacher-form");
 const studentForm = document.getElementById("student-form");
+
+function getData() {
+  $.ajax({
+    url: 'getDataClass.php',
+    type: 'POST',
+    success: function (data) {
+      if (data != 'false') {
+        var obj = JSON.parse(data);
+        var name = obj.name;
+        // var classs = obj.class;
+        // var nameList = "";
+        // var classList = "";
+        // for (var i = 0; i < name.length; i++) {
+        //     nameList += "<li>" + name[i] + "</li>";
+        // }
+        // for (var i = 0; i < classs.length; i++) {
+        //     classList += "<li>" + classs[i] + "</li>";
+        // }
+        // document.getElementById("student-form").innerHTML = "<h2>Name</h2><div class='dropdown'><div class='select'><span class='selected' id='name'>" + name[0] + "</span><div class='caret'></div></div><ul class='menu'>" + nameList + "</ul></div><h2>Class</h2><div class='dropdown'><div class='select'><span class='selected' name='class' id='class'>" + classs[0] + "</span><div class='caret'></div></div><ul class='menu'>" + classList + "</ul></div>";
+      } else {
+        document.getElementById("class-dropdown").innerHTML = `<div class='select'>
+                  <span class='selected' name='class' id='class'>none</span>
+                  <div class="caret"></div>
+              </div>
+              <ul class="menu"><li class="active">none</li></ul>`
+      }
+    }
+  });
+}
+
 dropdowns.forEach((dropdown) => {
   const select = dropdown.querySelector(".select");
   const caret = dropdown.querySelector(".caret");
