6b5c624c1ccb0d5eac65f31c8d180a8d386ec8f6
ddddddddrrdd3 <108572893+ddddddddrrdd3@users.noreply.github.com>
2024-06-16 12:39:22 +0800

    redirect bug fix

--- public/js/login.js
+++ public/js/login.js
@@ -82,7 +82,7 @@ loginButton.onclick = () => {
     function (data, status) {
       if (status === "success") {
         if (data.success) {
-          window.location.href = "studentpick.php";
+          window.location.href = "pickbox.php";
         } else {
           // maybe can use html to display
           alert(data.message);
